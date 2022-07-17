f7db1e1a9bca668173b05b282f91bbfc128b00db
[Aashish Rai] <[2aashishrai@gmail.com]>
2022-07-17 10:03:49 +0545

    update email template

--- pages/api/contact-us.ts
+++ pages/api/contact-us.ts
@@ -2,6 +2,7 @@ import mailgun from "mailgun-js";
 import { NextApiRequest, NextApiResponse } from "next";
 import { FormData } from "TYPES/FormData";
 import { appUseCors } from "middlewares/appUseCors";
+import getEmailTemplate from "backend/components/EmailTemplate";
 
 export default async function handler(
   req: NextApiRequest,
@@ -34,123 +35,17 @@ export default async function handler(
         message,
       }: FormData = req.body;
       //Mail Body being composed with the data being provided by the user through form
-      const emailText = `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Email</title>
-
-    <style>
-        html{
-            font-family: Poppins, Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-        }
-        body{
-            margin: 1rem;
-            background-color: #eee;
-
-            color: #333;
-
-            text-align: center;
-        }
-        h1{
-            font-size: 24px;
-            font-weight: 600;
-            color: #444;
-
-            /*background-color: rgba(95,36,133,1);*/
-        }
-        table{
-            margin: auto;
-            background: rgb(196,195,224);
-            background-color: #fff;
-
-            color: #333;
-            padding:5px;
-
-            font-size: 16px;
-            text-align: left;
-
-            border-radius: 10px;
-
-            box-shadow: 0 0 10px #44444455 ;
-        }
-        table tr:first-child{
-            font-size: 18px;
-            font-weight: 700;
-        }
-        table td{
-            padding: 10px 16px 10px 16px;
-        }
-        .email-container{
-            padding: 2px;
-        }
-    
-    </style>
-</head>
-<body>
-    <div class="email-container">
-        <h1>New Client Request</h1>
-        <table>
-            <tr>
-                <td><p>#</p></td>
-                <td><p>Items</p></td>
-                <td><p>Details</p></td>
-            </tr>
-            <tr>
-                <td><p>1.</p></td>
-                <td><p>Name of the requester</p></td>
-                <td><p>${name}</p></td>
-            </tr>
-            <tr>
-                <td><p>2.</p></td>
-                <td><p>For whome is this request for?</p></td>
-                <td><p>${_for}</p></td>
-            </tr>
-            <tr>
-                <td><p>3.</p></td>
-                <td><p>Services</p></td>
-                <td><ol>${services.map(
-                  (service) => "<li>" + service + "</li>"
-                )}</ol></td>
-            </tr>
-            <tr>
-                <td><p>4.</p></td>
-                <td><p>Phone Number</p></td>
-                <td><p>${phoneNumber}</p></td>
-            </tr>
-            <tr>
-                <td><p>5.</p></td>
-                <td><p>Email address</p></td>
-                <td><p>${email}</p></td>
-            </tr>
-            <tr>
-                <td><p>6.</p></td>
-                <td><p>Street</p></td>
-                <td><p>${street}</p></td>
-            </tr>
-            <tr>
-                <td><p>7.</p></td>
-                <td><p>Suburb</p></td>
-                <td><p>${suburb}</p></td>
-            </tr>
-            <tr>
-                <td><p>8.</p></td>
-                <td><p>Postcode</p></td>
-                <td><p>${postCode}</p></td>
-            </tr>
-             <tr>
-                <td><p>10.</p></td>
-                <td><p>Message</p></td>
-                <td><p>${message ?? "no messages"}</p></td>
-            </tr>
-        </table>
-    </div>
-</body>
-                                </html>`;
+      const emailBody = getEmailTemplate({
+        _for,
+        services,
+        name,
+        phoneNumber,
+        email,
+        street,
+        suburb,
+        postCode,
+        message,
+      });
 
       //Creating the auth object
       const mailGun = () =>
@@ -167,7 +62,7 @@ export default async function handler(
             from: "Interested Person <jhon@mg.yourdomain.com>", // sender address
             to: `${process.env.MAILGUN_RECEIVER_EMAIL}`, //email address of the receiver
             subject: "WoH Service Contact", // Subject line
-            html: emailText, // html body
+            html: emailBody, // html body
           },
           (error, body) => {
             if (error)
